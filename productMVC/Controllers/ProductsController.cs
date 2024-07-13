840753c4c0be2e0a92b18aabb1ef59d786f5e96d
ConKienHuy <kiengianghuy1989@gmail.com>
2024-07-13 17:57:32 +0700

    Committed

---
+++ productMVC/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using productMVC.Services;
+
+namespace productMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
---
+++ productMVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using productMVC.Models;
+
+namespace productMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+
+        }  
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
